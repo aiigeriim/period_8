6f6c8f44d158c1c0864a4f5161e75cf02f32312c
Aiigeriim <sardarbekova1005@gmail.com>
2025-09-25 21:58:55 +0300

    фикстуры\пагинация\поиск

--- webapp/views/project.py
+++ webapp/views/project.py
@@ -1,6 +1,10 @@
+from urllib.parse import urlencode
+
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from webapp.forms import TaskForm, SearchForm
 from webapp.forms.project import ProjectForm
 from webapp.models import Project, Task
 
@@ -10,6 +14,34 @@ class ProjectList(ListView):
     context_object_name = "projects"
     model = Project
     ordering = '-created_at'
+    paginate_by = 5
+    paginate_orphan = 1
+
+    def dispatch(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        result = super().get_context_data(**kwargs)
+        result["search_form"] = self.form
+        if self.search_value:
+            result['query'] = urlencode({'search': self.search_value})
+            result['search'] = self.search_value
+        return result
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
 
 
 class ProjectDetail(DetailView):
--- webapp/views/task.py
+++ webapp/views/task.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from webapp.forms import TaskForm
 from webapp.models import Task, Project
